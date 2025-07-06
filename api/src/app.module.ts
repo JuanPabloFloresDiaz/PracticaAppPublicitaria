3b484507d56e4f4962a91f9d24b361bbb3afd381
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-07-06 00:39:18 -0600

    ✨ Añadir módulo, controlador y servicio para la gestión de campañas

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CampaignsModule } from './campaigns/campaigns.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    }),],
+    }),
+    CampaignsModule,],
   controllers: [AppController],
   providers: [AppService],
 })
