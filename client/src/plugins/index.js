a7a1fcd2fe549ecff815c2d0388988609e58a101
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-07-06 22:41:12 -0600

    ✨ Añadir VueQueryPlugin y configurar QueryClient en el registro de plugins

--- client/src/plugins/index.js
+++ client/src/plugins/index.js
@@ -5,13 +5,23 @@
  */
 
 // Plugins
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
 import vuetify from './vuetify'
 import pinia from '@/stores'
 import router from '@/router'
 
 export function registerPlugins (app) {
+  
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+      }
+    }
+  })
+
   app
     .use(vuetify)
     .use(router)
     .use(pinia)
+    .use(VueQueryPlugin, { queryClient })
 }
